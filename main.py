2a1d8b58347ebd0c2ce0f06ffc156b0e880e8b80
Brody Foster <brody.foster@gmail.com>
2023-04-27 01:27:11 -0400

    4/26/2023
    ooga

--- main.py
+++ main.py
@@ -1,149 +1,187 @@
 import csv
 import math
+from typing import TypeAlias, Dict, Tuple
+from collections import defaultdict
 
-# could probably use more classes, especially where i have the dict[tuple[str, str], tuple[float, int]] nonsense
-class Rating:
-    def __init__(self, movieName: str, score: int):
-        self.movieName = movieName
-        self.score = score
 
-# ````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
-# Biggest problems right now:
-# The number of ratings per edge is veryyyy low. 1.00006 low. Need to bring this up to > 30
-# Solutions: make the allowed_movies set much more constrictive, so we're only considering widely reviewed, popular top_movies
-# ???
-# ````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+'''
+outline:
+
+read in ratings to get movie set, review data.
+    dict of [movie_id -> average_score, rating_count]
+    edge_list:
+        dict of users -> to, from, weight
+read in movie_data to generate filtered movie_data
+'''
+
+Rating = tuple[str, int]
+Edge = tuple[str, str, float]
+
+edge_list : [Edge]
+movies : []
+
+
+
+
 def main():
-    print("Reading in list of top movies...")
-    top_movies = get_top_movie_set("dsaproj3/modified_inputs/top_movie_list.csv")
-    #print("Top " + str(len(movies)) + " movies:\n")
-    #print(movies)
-
-    print("Reading in ratings...")
-    users = read_in_ratings('dsaproj3/letterboxd_database/ratings_export.csv', allowed_movies=top_movies, max_size=1000, modulo=10) 
-
-    #for userID in users:
-    #    print("User: " + userID + ", Reviews: " + str(len(users[userID])))
-
-    print("Creating edges...")
-    edges = create_edge_list(users)
-
-    print("Writing edges to file...")
-    create_edge_file("output/edges.csv", edges)
-
-    print("Done!")
-    print("Average reviews per edge: " + str(count_average_ratings_per_edge(edges)))
+    print("Aggregating allowed_movies...")
+    allowed_movie_set = get_allowed_movies()
+    print("Reading in user ratings...")
+    movies, users = read_in_ratings('letterboxd_database/ratings_export.csv', allowed_movie_set)
+    #print("Constructing new movie_data.csv...")
+    #write_new_movie_data_csv(allowed_movie_set, movies)
+    print("Constructing edge list...")
+    edges = create_edge_list(users, movies, allowed_movie_set)
+    print("Writing edges to edges.csv...")
+    write_edge_list_csv(edges, 'output/edges.csv')
 
 
+def get_allowed_movies() -> set[str]:
+    movie_set = set()
+    with open('output/significant_movies.csv', "r") as file:
+        for line in file:
+            movie_set.add(line.strip())
 
-def calculate_edge_weight_per_rating(from_rating : Rating, to_rating : Rating) -> float:
-    # TODO: incorporate a movie's average rating so the equation looks like abs( (to_rating - to_average) - (from_rating - from_average) )
-    return abs(float(to_rating.score) - float(from_rating.score)) 
+    return movie_set
 
-def count_average_ratings_per_edge(edges):
-    average = 0.0
-    i = 0.0
-    for edge in edges:
-        i+=1
-        average += edges[edge][1] 
-    return average/i
+def read_in_ratings(filepath: str, allowed_movie_set: set[str]) -> (dict[str, tuple[float, int]], dict[str, list[Rating]]):
 
+    movies : dict[str, tuple[float, int]]
+    movies = {}
 
-def create_edge_list(users: dict[str, list[Rating]]) -> dict[tuple[str, str], tuple[float, int]]:
-
-    edges : dict[tuple[str, str], tuple[float, int]] # tuple of to, from movieID to weight and number of edges
-    edges = {}
-
-    # loop through all of a user's ratings, connecting all movies they've rated
-    for userID in users:
-        for fromRating in users[userID]:
-            for to_rating in users[userID]:
-
-                # identify the pair of movies
-                fromMovieID = fromRating.movieName
-                toMovieID = to_rating.movieName
-                # break we're connecting a movie to itself
-                if fromMovieID == toMovieID : break
-                vertexPair = (fromMovieID, toMovieID)
-
-                # calculate this user's connection's weight, will be averaged in to the edge weight.
-                ratingWeight = calculate_edge_weight_per_rating(fromRating, to_rating)
-
-                # add the vertexPair key to the dict if this is the first connection between these two vertexes
-                if not vertexPair in edges:
-                    edges[vertexPair] = (0.0, 0)
-
-                # weird tuple fenangling because i shouldn't have used a tuple for the weight/number of ratings
-                numRatings = edges[vertexPair][1]
-                # averages this rating into the edges' weight
-                edges[vertexPair] = ( (edges[vertexPair][0] * numRatings + ratingWeight) / (numRatings + 1) , numRatings + 1)
-    return edges
-
-
-
-def create_edge_file(filepath: str, edges: dict[tuple[str, str], tuple[float, int]]):
-
-    # TODO: this writes the file all at once using one string, which is probably why it takes for-fucking-ever to run. feel free to optimize!
-    text = "Source,Target,Weight\n"
-    i = 0.0
-    size = len(edges)
-    print("Writing " + str(len(edges)) + " edges...")
-    for edge in edges:
-        fromMovie = edge[0]
-        toMovie = edge[1]
-        weight = edges[edge][0]
-        text += fromMovie + "," + toMovie + "," + str(weight) + "\n"
-
-        # silly little progress counter
-        if (i % 1000) == 0:
-            print(str(i/size * 100)+'%')
-        i += 1
-
-
-    with open('dsaproj3/output/edges.csv', 'w') as file:
-        file.write(text)
-        
-
-# reads all ratings into a dictionary mapping usernames to a list of their ratings
-# allowed_movies, if passed, is a set of movies that the reader will check against, discarding all reviews not in it.
-def read_in_ratings(filepath: str, allowed_movies = None, max_size = 1000000, modulo = 1) -> dict[str, list[Rating]]: 
     users : dict[str, list[Rating]]
     users = {}
 
     with open(filepath, newline='', encoding="utf8") as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-        i = 0
+        next(reader) # skip header
         for row in reader:
-            i += 1
-            # if the max number of reviews has been reached, break
-            if i >= max_size * modulo: break
-            # optional modulo argument allows to take every nth review. useful for getting a wider distribution of users. (modulo = 3 ~> only every third review is added)
-            if not (i % modulo == 0): continue
-            # grab data from csv
-            movieID = row[1]
-            ratingVal = row[2]
-            userID = row[3]
-            # if the movie is not in the given set of allowed movies, discard the review (added clause for no set provided)
-            if not (allowed_movies == None or movieID in allowed_movies): 
-                continue
-            # add the user to the dictionary if this is their first review
-            if not userID in users:
-                users[userID] = []
-            # add the review to their list of rating
-            users[userID].append(Rating(movieID, ratingVal))
-    return users
+            if not row[1] in allowed_movie_set:
+                continue 
+            movie_id = row[1]
+            rating_value = int(row[2])
+            user_id = row[3]
+
+            # add the review to the dictionary of movies, updating the total number of reviews and the average score
+            if movie_id in movies:
+                count = movies[movie_id][1]
+                new_avg = float(movies[movie_id][0] * count + rating_value) / (count + 1)
+                movies[movie_id] = (new_avg, count + 1)
+            else:
+                movies[movie_id] = (rating_value, 1)
 
 
-# method to read in a list of movies, i manually created the file by sorting the movie_data.csv by popularity and then pruning the top 50,000 or something.
-def get_top_movie_set(filepath: str) -> set[str]:
-    movies : set[str]
-    movies = set()
-    with open(filepath, newline='', encoding="utf8") as csvfile:
+            # add the review to the dictionary of users, which will then be used to create the edge list
+            if user_id in users:    
+                users[user_id].append((movie_id, rating_value))   
+            else:
+                users[user_id] = []
+                users[user_id].append((movie_id, rating_value))  
+    return (movies, users)
+
+def write_new_movie_data_csv(allowed_movie_set, movies):
+    # read in file
+    data : list(str, str, float, int, str)
+    data = []
+    with open('letterboxd_database/movie_data.csv', newline='', encoding="utf8") as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
+        next(reader)
         for row in reader:
-            movies.add(row[0])
-    return movies
+            try:
+                if (not row[5] in allowed_movie_set):
+                    continue
+            except: 
+                print("exception: " + row[5])
+                continue
+            movie_id = row[5]
+            movie_title = row[6]
+            image_url = 'https://a.ltrbxd.com/resized/' + row[2] + '.jpg'
+            average_rating = movies[movie_id][0]
+            rating_count = movies[movie_id][1]
+            data.append((movie_id, movie_title, image_url, average_rating, rating_count))
 
 
-if __name__ == "__main__":
-    main()
+    # write the contents to file
+    path = 'output/filtered_movie_data.csv'
+    with open(path, 'w', newline='', encoding="utf8") as csvfile:
+        # Create a CSV writer
+        writer = csv.writer(csvfile)
+
+        # Write the header row
+        writer.writerow(['Movie ID', 'Movie Name', 'Image Link', 'Average Score', 'Review Count'])
+
+        # Write each row of data
+        for row in data:
+            try:
+                writer.writerow(row)
+            except:
+                print("error: " + row)
+def create_edge_list(users, movies, allowed_movie_set):
+    edges : dict[tuple[str, str], tuple[float, int]] # dict of vertex pairs to edge weight, and count
+    edges = defaultdict(lambda: [0.0, 0])
+
+    # loop through all of a user's ratings, connecting all movies they've rated
+    total = len(users)
+    print(total)
+    user_count = 0
+    for user_id in users:
+        user_count+=1
+        print(user_count)
+        # loop through every review a user has
+        for i in range(len(users[user_id])):
+            # loop through every other review
+            for j in range(i + 1, len(users[user_id])):
+                from_rating = users[user_id][i]
+                to_rating = users[user_id][j]
+
+                # identify the pair of movies 
+                from_movie_id = from_rating[0]
+                to_movie_id = to_rating[0]
+                
+                vertex_pair = (from_movie_id, to_movie_id)
+
+                # calculate the weight of this connection from this user. will be averaged into overall edge weight
+                rating_weight = calculate_edge_weight_per_rating(from_rating, to_rating, movies)
+ 
+                
+                # average the rating into the dictionary
+                num_ratings = edges[vertex_pair][1]
+                current_weight = edges[vertex_pair][0]
+                new_weight = (current_weight * num_ratings + rating_weight) / (num_ratings + 1)
+                edges[vertex_pair] = (new_weight, num_ratings + 1)
+    return edges
+
+
+
+
+def write_edge_list_csv(edges, file_name):
+    with open(file_name, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Source', 'Target', 'Weight', 'Count'])
+        for edge, data in edges.items():
+            to_name = fix_movie_name(edge[0])
+            from_name = fix_movie_name(edge[1])
+            writer.writerow([to_name, from_name, data[0], data[1]])
+
+
+def calculate_edge_weight_per_rating(from_rating, to_rating, movies) -> float:
+    to_average_rating_value = movies[to_rating[0]][0]
+    from_average_rating_value = movies[from_rating[0]][0]
+
+    weight = abs((to_rating[1] - to_average_rating_value) - (from_rating[1] - from_average_rating_value))
+    return weight
+
+def fix_movie_name(name):
+    name = name.capitalize()
+    fixed = ''
+    for i in range(0, len(name)):
+        if name[i] == "-":
+            fixed = fixed + " "
+        elif name[i-1] == "-":
+            fixed = fixed + name[i].upper()
+        else:
+            fixed = fixed + name[i]
+
+    return fixed
+
+main()
